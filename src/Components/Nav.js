c1cc6d80795b81e56dc49f547d97514f7258d140
JadenFicklin <jadendficklin@gmail.com>
2022-08-05 20:44:37 -0600

    styling nav bar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -8,14 +8,15 @@ function Nav() {
         <div className="middle"></div>
         <div className="right">
           <div className="about">
-            <span className="number">01.</span>About
+            <span className="number">01. </span>About
           </div>
           <div className="work">
-            <span className="number">02.</span>Work
+            <span className="number">02. </span>Work
           </div>
           <div className="contact">
-            <span className="number">03.</span>Contact
+            <span className="number">03. </span>Contact
           </div>
+          <div className="resume"> Resume</div>
         </div>
       </nav>
     </>
