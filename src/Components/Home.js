14fd01a79943886a99695ee9ff8d0f3b74edf05e
JadenFicklin <jadendficklin@gmail.com>
2022-08-05 21:05:11 -0600

    added some text to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,17 @@ function Home() {
   return (
     <>
       <Nav />
-      <div className="home">home</div>
+      <div className="home">
+        <div className="intro">
+          <h5 className="introduction">Hello, my name is</h5>
+          <div className="name">Jaden Ficklin.</div>
+          <div className="occupation">I am a Web Developer.</div>
+          <div className="small-intro">
+            I’m a junior full stack engineer specializing in the react
+            framework, though <br></br>open to new frameworks.
+          </div>
+        </div>
+      </div>
     </>
   );
 }
