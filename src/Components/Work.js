31ab266cb95aef1df65bfaeca50357adfe282642
JadenFicklin <jadendficklin@gmail.com>
2022-08-17 13:12:23 -0600

    text for work

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -13,7 +13,14 @@ function Work() {
             <div className="euka-right">
               <div className="euka-project-name">Featured Project</div>
               <div className="euka-name">Euka</div>
-              <div className="euka-box">Euka is</div>
+              <div className="euka-box">
+                Euka is an ecommerce website designed to market shoes. <br></br>
+                <br></br>
+                <span className="number">timeframe of build: </span> 2 weeks{" "}
+                <br></br>
+                <br></br>{" "}
+                <span className="number">main focus of website: </span> CSS
+              </div>
               <div className="euka-languages-used">
                 CSS React HTML Javascript
               </div>
@@ -24,7 +31,16 @@ function Work() {
             <div className="sopi-left">
               <div className="sopi-project-name">Featured Project</div>
               <div className="sopi-name">Sopi</div>
-              <div className="sopi-box">Sopi is</div>
+              <div className="sopi-box">
+                {" "}
+                Sopi is a socializing website designed to upload and rate
+                videos. <br></br>
+                <br></br>
+                <span className="number">timeframe of build: </span> 2 weeks{" "}
+                <br></br>
+                <br></br>
+                <span className="number">main focus of website: </span>SQL
+              </div>
               <div className="sopi-languages-used">
                 CSS React HTML Javascript SQL
               </div>
@@ -37,7 +53,16 @@ function Work() {
             <div className="ambience-right">
               <div className="ambience-project-name">Featured Project</div>
               <div className="ambience-name">Ambience</div>
-              <div className="ambience-box">Ambience is</div>
+              <div className="ambience-box">
+                Ambience is a website developed to adjust enjoyable ambient
+                sounds. <br></br>
+                <br></br>
+                <span className="number">timeframe of build: </span> 1 week{" "}
+                <br></br>
+                <br></br>{" "}
+                <span className="number">main focus of website: </span> reactive
+                CSS
+              </div>
               <div className="ambience-languages-used">
                 CSS React HTML Javascript
               </div>
