d0cf2f6a372af92d21059db1884ff007e5679b2c
JadenFicklin <jadendficklin@gmail.com>
2022-08-18 12:28:31 -0600

    introduction hover update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -18,8 +18,40 @@ function Home() {
       <div className="home">
         <div className="intro">
           <h5 className="introduction">Hello, my name is</h5>
-          <div className="name">Jadenn Ficklin.</div>
-          <div className="occupation">I am a Web Developer.</div>
+          <div className="name">
+            <span className="l">J</span>
+            <span className="l">a</span>
+            <span className="l">d</span>
+            <span className="l">e</span>
+            <span className="l space">n</span>
+            <span className="l">F</span>
+            <span className="l">i</span>
+            <span className="l">c</span>
+            <span className="l">k</span>
+            <span className="l">l</span>
+            <span className="l">i</span>
+            <span className="l">n</span>
+            <span className="l">.</span>
+          </div>
+          <div className="occupation">
+            <span className="l space">I</span>
+            <span className="l">a</span>
+            <span className="l space">m</span>
+            <span className="l space">a</span>
+            <span className="l">W</span>
+            <span className="l">e</span>
+            <span className="l space">b</span>
+            <span className="l">D</span>
+            <span className="l">e</span>
+            <span className="l">v</span>
+            <span className="l">e</span>
+            <span className="l">l</span>
+            <span className="l">o</span>
+            <span className="l">p</span>
+            <span className="l">e</span>
+            <span className="l">r</span>
+            <span className="l">.</span>
+          </div>
           <div className="small-intro" onClick={() => (scrollPosition = 0)}>
             I’m a junior full stack engineer specializing in the react
             framework, though <br></br>open to new frameworks.
