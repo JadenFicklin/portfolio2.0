48068e2dbc24b7ccf15fe8fa0256a6e7e8d998be
JadenFicklin <jadendficklin@gmail.com>
2022-09-26 11:26:18 -0600

    deletes all pictures

--- src/assets/index.js
+++
@@ -1,6 +0,0 @@
-export const ambience = require("./ambience.jpg");
-export const daccord = require("./daccord.jpg");
-export const euka = require("./euka.png");
-export const jaden = require("./jaden.jpg");
-export const sopi = require("./sopi.jpg");
-export const wave = require("./wave.svg");
