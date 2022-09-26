6769db7504b88b9d4ac4ed08ffd743e0b515c195
JadenFicklin <jadendficklin@gmail.com>
2022-09-26 11:27:27 -0600

    restores images

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export const ambience = require("./ambience.jpg");
+export const daccord = require("./daccord.jpg");
+export const euka = require("./euka.png");
+export const jaden = require("./jaden.jpg");
+export const sopi = require("./sopi.jpg");
+export const wave = require("./wave.svg");
