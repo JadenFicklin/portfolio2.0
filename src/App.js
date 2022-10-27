e36fa5d3dfae287c58f46bdfec56824863ddc2e2
JadenFicklin <jadendficklin@gmail.com>
2022-10-27 13:22:42 -0600

    added tab name

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 import Home from "./Components/Home";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Jaden Ficklin Portfolio</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Jaden Ficklin Portfolio" />
+      </Helmet>
       <Home />
     </>
   );
